0a7b5f968bc1dd6832f6df6dda9696a2c53d8085
wasanjith <wasakodi79@gmail.com>
2024-10-30 10:45:46 +0530

    Create Pages and setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '../src/pages/Home'
+import Collection from '../src/pages/Collection'
+import About from '../src/pages/About'
+import Contactc from '../src/pages/Contact'
+import Product from '../src/pages/Product'
+import Cart from '../src/pages/Cart'
+import Login from '../src/pages/Login'
+import PlaceHolder from '../src/pages/PlaceHolder'
+import Orders from '../src/pages/Orders'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path = '/' element = {<Home/>} /> 
+        <Route path = '/collection' element = {<Collection/>} />
+        <Route path = '/about' element = {<About/>} />
+        <Route path = '/contact' element = {<Contactc/>} />
+        <Route path = '/product/:productId' element ={<Product/>} />
+        <Route path = '/cart' element = {<Cart/>} />
+        <Route path = '/login' element = {<Login/>} />
+        <Route path = '/place-holder' element = {<PlaceHolder/>} />
+        <Route path = '/orders' element = {<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/pages/PlaceHolder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlaceHolder = () => {
+  return (
+    <div>PlaceHolder</div>
+  )
+}
+
+export default PlaceHolder
