6aecd7ea4ed4a637401b160cfcb7971e983a56c2
wasanjith <wasakodi79@gmail.com>
2024-12-12 20:49:03 +0530

    Successfully created contac us page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewLatestBox from '../components/NewLatestBox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+          <img className='w-[30%] md:max-w[480px]' src={assets.contact_img} alt="" />
+          <div className='flex flex-col justify-center items-start gap-6'>
+            <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+            <p className='text-gray-500'>54709 willms Station <br /> Suite 350, Washington, USA</p>
+            <p className='text-gray-500'>Tel (011) 555-12-345-10 <br /> Email: pheeew@gmail.com</p>
+            <p className='font-semibold text-xl text-gray-600'>Careers at PHEEEW</p>
+            <p className='text-gray-500'> Learn more about our teams and job openings.</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          </div>
+      </div>
+      <NewLatestBox/>
+    </div>
   )
 }
 
