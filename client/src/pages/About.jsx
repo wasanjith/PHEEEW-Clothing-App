8cc361932d89e00f5e45454a48c196d3a96993a5
wasanjith <wasakodi79@gmail.com>
2024-12-12 20:30:23 +0530

    Successfully created about us page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import Title from '../components/Title'
 import {assets} from '../assets/frontend_assets/assets'
@@ -7,7 +8,7 @@ import NewLatestBox from '../components/NewLatestBox'
 const About = () => {
   return (
     <div>
-      <div className='text-2xl text-center pt-8 border-t'>
+      <div className='text-4xl text-center pt-8 border-t'>
           <Title text1={'ABOUT'} text2={'US'}/>
       </div>
       <div className='my-10 flex flex-col md:flex-row gap-16'>
@@ -40,31 +41,32 @@ const About = () => {
         </div>
       </div>
           
-      <div className='text-3xl py-8 mt-20'>
+      <div className='text-2xl py-8 mt-20'>
           <Title text1={'WHY'} text2={'CHOOSE US'}/>
           <div className='flex flex-col md:flex-row text-sm mb-20 '>
               <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-                  <b className='text-xl' >Quality Assurance :</b>
+                  <b className='text-xl text-gray-500' >Quality Assurance :</b>
                   <p className='text-gray-600'>We meticulosly select and vet each product to ensure it meets our stringment quality standards.</p>
               </div>
               <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-                  <b className='text-xl'>Convenience :</b>
+                  <b className='text-xl text-gray-500 '>Convenience :</b>
                   <p className='text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
               </div>
               <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
-                  <b className='text-xl'>Exceptional Customer Service :</b>
+                  <b className='text-xl text-gray-500'>Exceptional Customer Service :</b>
                   <p className='text-gray-600'>Our team of dedicated professionals iis here to assist you the way, ensuring  your satisfaction is our top prioroty. </p>
               </div>
           </div>
       </div>
       <div className='flex flex-row justify-between py-8 gap-16'>
                         <div className=''>
-                              <b className='items-center justify-center text-2xl'>Our Mission</b>
+                          <div className='text-2xl'><Title  text1={'OUR'} text2={'MISION'}/></div>
+                        
                               <hr />
                               <p className='text-gray-600 py-4 text-justify'>At PHEEEW, our mission is to inspire confidence and self-expression through fashion. We are dedicated to crafting high-quality, sustainable clothing that combines style, comfort, and affordability. Our goal is to empower individuals to embrace their unique identity and feel confident in every aspect of their lives.</p>
                         </div>
               <div>
-                    <b className='text-2xl'>Our Vision</b>
+                    <div className='text-2xl'><Title text1={'OUR'} text2={'VISION'}/></div>
                     <hr />
                       <p className='text-gray-600 py-4 text-justify'>Our vision is to become a global leader in the fashion industry by setting new standards for innovation, sustainability, and inclusivity. We envision a world where everyone has access to clothing that not only reflects their personal style but also contributes to a more sustainable and equitable future.</p>
                </div>
