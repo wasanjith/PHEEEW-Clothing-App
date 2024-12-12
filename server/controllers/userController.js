4a80f5b42e2bc6f3a783a43ae3f3eaf97e993ece
wasanjith <wasakodi79@gmail.com>
2024-12-13 00:25:45 +0530

    Successfully created login function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,7 +45,26 @@ const registerUser = async (req, res) => {
 };
 
 // Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false, message: "User not found"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(isMatch){
+            const token = createToken(user._id);
+            res.json({success:true, token});
+        }
+        else{
+            res.json({success:false, message: "Invalid credentials"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
+};
 
 // Route for admin login
 const adminLogin = async (req, res) => {};
