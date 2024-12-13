9deed672c4a8a22b9816c44276b67f8837a69499
wasanjith <wasakodi79@gmail.com>
2024-12-14 00:49:38 +0530

    successfully created login to admin page with authentication

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar'
 import SideBar from './components/SideBar'
 import { Route , Routes } from 'react-router-dom'
-import Add from './components/Add'
-import List from './components/List'
-import Orders from './components/Orders'  
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'  
+import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+
+  const [token, setToken] = useState('');
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-      <NavBar /> 
-      <hr />
-      <div className='flex w-full'>
-      <SideBar />
-      <div className='w-[70-%] mx-auto ml-[max(5vw,25px) my-8 text-gray-600 text-base]'>
-          <Routes>
-              <Route path='/add' element={<Add />} />
-              <Route path='/list' element={<List />} />
-              <Route path='/orders' element={<Orders />} />
-          </Routes>
-      </div>
-      </div>
-      </>
+      <ToastContainer />
+      { token === "" 
+      ? <Login setToken={setToken}/> 
+      : 
+          <>
+          <NavBar /> 
+          <hr />
+          <div className='flex w-full'>
+          <SideBar />
+          <div className='w-[70-%] mx-auto ml-[max(5vw,25px) my-8 text-gray-600 text-base]'>
+              <Routes>
+                  <Route path='/add' element={<Add />} />
+                  <Route path='/list' element={<List />} />
+                  <Route path='/orders' element={<Orders />} />
+              </Routes>
+          </div>
+          </div>
+          </>
+      }
+      
     </div>
   )
 }
