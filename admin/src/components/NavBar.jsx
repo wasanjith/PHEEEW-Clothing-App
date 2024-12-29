88461b2d68386cf92137aff930a97d44865630fa
wasanjith <wasakodi79@gmail.com>
2024-12-30 04:05:46 +0530

    In the admin site add page create successfully..

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/admin_assets/assets'
 
-const NavBar = () => {
+
+const NavBar = ({setToken}) => {
+  
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img className='w-[10%]' src={assets.logo} alt="" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={()=> setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
