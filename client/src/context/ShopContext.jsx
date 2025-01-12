8b5fa2d61a77d110328e60768c216ac0fe7fcbf0
wasanjith <wasakodi79@gmail.com>
2025-01-12 19:39:12 +0530

    dheera's edit

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -5,120 +5,119 @@ import axios from "axios";
 
 export const ShopContext = createContext();
 
-
-
 const ShopContextProvider = (props) => {
-    
-    const currency = '$';
-    const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const [search, setSearch] = useState('');
-    const [showSearch , setShowSearch] = useState(false);
-    const [cartItems, setCartItems] = useState({});
-    const [products, setProducts] = useState([]);
-    const navigate = useNavigate();
+  const currency = "$";
+  const delivery_fee = 10;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    
-
-    const addToCart = async (itemId,size) => {
-
-        if(!size){
-            toast.error('Select Product Size');
-            return;
-        }
-      
-        let cartData = structuredClone(cartItems);
-        if(cartData[itemId]){
-            if(cartData[itemId][size]){
-                cartData[itemId][size] += 1;
-            }
-            else{
-                cartData[itemId][size] = 1;
-            }
-        }
-        else{
-            cartData[itemId] = {};
-            cartData[itemId][size] = 1;
-        }
-        setCartItems(cartData);
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
     }
 
-    const getCardCount = () => {
-        let totalcount = 0;
-        for(const items in cartItems){
-            for(const item in cartItems[items]){
-                try {
-                    if (cartItems[items][item] > 0) {
-                        totalcount += cartItems[items][item];
-                    }
-                } catch (error) {
-                    
-                }
-            }
-        }
-        return totalcount;
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
     }
+    setCartItems(cartData);
+  };
 
-    const updateQuantity = async (itemId, size, quantity) => {
-        let cartData = structuredClone(cartItems);
-        cartData[itemId][size] = quantity;
-        setCartItems(cartData);
+  const getCardCount = () => {
+    let totalcount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalcount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
     }
+    return totalcount;
+  };
 
-    const getCartAmount = () => {
-        let totalAmount = 0;
-        for(const items in cartItems){
-            let itemInfo = products.find((product) => product._id === items);
-            for(const item in cartItems[items]){
-                try {
-                    if(cartItems[items][item] > 0){
-                        totalAmount += itemInfo.price * cartItems[items][item];
-                    }
-                } catch (error) {
-                    
-                }
-            }
-        }
-        return totalAmount;
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {}
+      }
     }
+    return totalAmount;
+  };
 
-    const getProductsData = async () => {
-        try{
-            
-            const response = await axios.get(backendUrl + '/api/product/list')
-            console.log(response.data);
-            if(response.data.success){
-                setProducts(response.data.products);
-            }else{
-                console.log('Error', response.data.message);
-                toast.error(response.data.message);
-            }
-        } catch (error){
-            console.log('Error', error);
-            toast.error(error.message);
-        }
+  const getProductsData = async () => {
+    try {
+      var token;
+      token = localStorage.getItem("token");
+
+      const response = await axios.get(backendUrl + "/api/product/list", {
+        headers: {
+          "Content-Type": "application/json",
+          token: `${token}`,
+        },
+      });
+      console.log(response.data);
+      if (response.data.success) {
+        console.log("products recieved");
+        setProducts(response.data.products);
+      } else {
+        console.log("Error", response.data.message);
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log("Error", error);
+      toast.error(error.message);
     }
+  };
 
-    useEffect(() => {
-        getProductsData();
-    },[]);
+  useEffect(() => {
+    getProductsData();
+  }, []);
 
-    const value ={
-        products , currency , delivery_fee,
-        search, setSearch, showSearch , setShowSearch,
-        cartItems, addToCart,
-        getCardCount, updateQuantity,
-        getCartAmount,
-        navigate,
-        backendUrl
-    }
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItems,
+    addToCart,
+    getCardCount,
+    updateQuantity,
+    getCartAmount,
+    navigate,
+    backendUrl,
+  };
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
-  
-
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,24 +1,24 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import productModel from '../models/productModel.js';
 
 
 // function for add product
-const addProduct = async (req, res) => {
+const addProduct = async(req, res) => {
     try {
-        const {name, description, price, category, subCategory, sizes, bestSeller, date} = req.body;
+        const { name, description, price, category, subCategory, sizes, bestSeller, date } = req.body;
         const image1 = req.files.image1 && req.files.image1[0];
         const image2 = req.files.image2 && req.files.image2[0];
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
 
-        const images = [image1, image2, image3, image4].filter((item) =>item !== undefined)
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
         let imageUrl = await Promise.all(
-            images.map(async (item) => {
-                let result = await cloudinary.uploader.upload(item.path,{resource_type: 'image'});
+            images.map(async(item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
                 return result.secure_url;
             })
         )
-        
+
         const productData = {
             name,
             description,
@@ -34,46 +34,45 @@ const addProduct = async (req, res) => {
         console.log(productData);
         const product = new productModel(productData);
         await product.save();
-        res.json({success: true, message: "Product added successfully"});
-        
+        res.json({ success: true, message: "Product added successfully" });
+
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message});
+        res.json({ success: false, message: error.message });
     }
 };
 // function for list products
-const listProducts = async (req, res) => {
+const listProducts = async(req, res) => {
     try {
         const products = await productModel.find({});
-        res.json({success:true, products})
+        res.json({ success: true, products })
     } catch (error) {
         console.log(error)
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 };
 // function for removing products
-const removeProduct = async (req, res) => {
+const removeProduct = async(req, res) => {
     try {
         await productModel.findByIdAndDelete(req.body.id);
-        res.json({success:true, message: "Product removed successfully"});
+        res.json({ success: true, message: "Product removed successfully" });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message});
+        res.json({ success: false, message: error.message });
     }
 };
 // function for single product products
-const singleProduct = async (req, res) => {
+const singleProduct = async(req, res) => {
     try {
-        const {productId}  = req.body
+        const { productId } = req.body
         const product = await productModel.findById(productId);
-        res.json({success:true, product});
-        
+        res.json({ success: true, product });
+
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message});
-        
+        res.json({ success: false, message: error.message });
+
     }
 };
 
 export { addProduct, listProducts, removeProduct, singleProduct };
-
